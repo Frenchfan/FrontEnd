a5335c6290f5c486997ba1ca825e54d65d7a3032
Frenchfan <sumkin.gpb@gmail.com>
2023-06-17 03:49:07 +0300

    Technical update

--- BackEnd/Sem3HTMLHW/hw3/src/main/java/com/example/reactive/REST/controller/CustomerController.java
+++ BackEnd/Sem3HTMLHW/hw3/src/main/java/com/example/reactive/REST/controller/CustomerController.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/gb/customer")
 public class CustomerController {
 
     @Autowired
--- BackEnd/Sem3HTMLHW/hw3/src/main/java/com/example/reactive/REST/controller/TemperatureController.java
+++ BackEnd/Sem3HTMLHW/hw3/src/main/java/com/example/reactive/REST/controller/TemperatureController.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/temperature")
+@RequestMapping("/gb/temperature")
 public class TemperatureController extends BaseController<Temperature, Integer, TemperatureRepository> {
 
     // @Autowired
