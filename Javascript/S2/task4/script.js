cade33e41ae5f233241868880f76391343899624
Frenchfan <sumkin.gpb@gmail.com>
2023-08-11 16:19:19 +0300

    Seminar2 - basic prompts and comparison

---
+++ Javascript/S2/task4/script.js
@@ -0,0 +1,21 @@
+// 1. Не запуская код, определите, что выводится в консоль:
+ 
+// ```
+// console.log(String(true));
+// console.log('a' + true); 
+// console.log(Number(true));
+// console.log(true + 1);
+// console.log(true + true);
+// console.log(true - true);
+// console.log(String(true) + Number(true));
+// ```
+ 
+// Тайминг: 20 минут.
+
+console.log(String(true));//true
+console.log('a' + true); //atrue
+console.log(Number(true));//1
+console.log(true + 1);//2
+console.log(true + true);//2
+console.log(true - true);//0
+console.log(String(true) + Number(true));//true1
